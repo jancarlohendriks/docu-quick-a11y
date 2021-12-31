bd5cbcc8327db21d6cb0e230715786808872401c
jancarlohendriks <jancarlohendriks@hotmail.com>
2021-12-31 14:08:58 +0100

    minor changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,28 +15,18 @@ const config = {
   favicon: 'img/favicon.ico',
   organizationName: 'jancarlo', // Usually your GitHub org/user name.
   projectName: 'docu-quick-a-11-y', // Usually your repo name.
-
+	themes: ['@docusaurus/theme-live-codeblock'],
   presets: [
     [
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-				// docs: {
-				// 	sidebarPath: require.resolve('./sidebars.js'),
-				// 	routeBasePath: '/'
-				// },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/jancarlohendriks/docu-quick-a11y/blob/main/',
         },
 				blog: false,
-        // blog: {
-        //   showReadingTime: true,
-        //   // Please change this to your repo.
-        //   editUrl:
-        //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -56,74 +46,15 @@ const config = {
 					// href: '/docs/welcome',
         },
         items: [
-          // {
-          //   type: 'doc',
-          //   docId: 'welcome',
-          //   position: 'left',
-          //   label: 'Welcome',
-          // },
-					// {
-          //   type: 'doc',
-          //   position: 'left',
-          //   docId: 'checklist',
-          //   label: 'Tools',
-          // },
           {to: '/docs/principles/basics', label: 'Principles', position: 'left'},
           {to: '/docs/get-started/color-contrast', label: 'Get Started', position: 'left'},
           {to: '/docs/tools/checklist', label: 'Tools', position: 'left'},
           {to: '/docs/library/menu', label: 'Library', position: 'left'},
-          // {to: '/docs/tools', label: 'Tools', position: 'left'},
-          // {to: '/blog', label: 'Blog', position: 'left'},
-          // {
-          //   href: 'https://github.com/facebook/docusaurus',
-          //   label: 'GitHub',
-          //   position: 'right',
-          // },
         ],
       },
       footer: {
         // style: 'dark',
-        links: [
-          // {
-          //   title: 'Docs',
-          //   items: [
-          //     {
-          //       label: 'Welcome',
-          //       to: '/docs/welcome',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'Community',
-          //   items: [
-          //     {
-          //       label: 'Stack Overflow',
-          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-          //     },
-          //     {
-          //       label: 'Discord',
-          //       href: 'https://discordapp.com/invite/docusaurus',
-          //     },
-          //     {
-          //       label: 'Twitter',
-          //       href: 'https://twitter.com/docusaurus',
-          //     },
-          //   ],
-          // },
-          // {
-          //   title: 'More',
-          //   items: [
-          //     {
-          //       label: 'Blog',
-          //       to: '/blog',
-          //     },
-          //     {
-          //       label: 'GitHub',
-          //       href: 'https://github.com/facebook/docusaurus',
-          //     },
-          //   ],
-          // },
-        ],
+        links: [],
         copyright: `Copyright © ${new Date().getFullYear()} Quick Accessibility Guide.`,
       },
       prism: {
